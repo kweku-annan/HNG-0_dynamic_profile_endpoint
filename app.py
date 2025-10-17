dfd00929d7de57860b252261f0bca61f9c21d72c
kweku-annan <kwekuannan.dev@gmail.com>
2025-10-17 06:44:10 +0000

    Everything works fine. Left with clean up and deployment

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from datetime import datetime, timezone
 
 from flask import Flask, jsonify
 
-profile = {
-    "status": "success",
-    "user": {
-        "email": "kwekuannan.dev@gmail.com",
-        "name": "Emmanuel Adu Saah",
-        "stack": ["Python", "Django", "Flask"]
-    },
-    "timestamp": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace('+00:00', 'Z'),
-    "fact": "Cats sleep a lot!"
-}
 
 app = Flask(__name__)
 
-@app.route('/me')
-def home():
-    return jsonify(profile)
+@app.route('/me', methods=['GET'])
+def me():
+    from models.profile import Profile
+    profile = Profile()
+    data = profile.profile_dict()
+
+    return jsonify(data), 200
 
 
 if __name__ == "__main__":
--- models/profile.py
+++ models/profile.py
@@ -1,5 +1,29 @@
 """A model with methods that sets my profile information"""
 from datetime import datetime, timezone
+import requests
+
+
+def get_facts():
+    """Fetches Cat Facts from external API"""
+    url = "https://catfact.ninja/fact"
+    try:
+        response = requests.get(url, timeout=(10, 10))
+
+        # Raise an HTTPError for bad responses
+        response.raise_for_status()
+
+        response_dict = response.json()
+        fact = response_dict['fact']
+        return fact
+    except requests.exceptions.Timeout:
+        return "The request timed out"
+    except requests.exceptions.ConnectionError:
+        return "Failed to connect. Check your internet connection"
+    except requests.exceptions.HTTPError as e:
+        return f"HTTP error occurred: {e}"
+    except requests.exceptions.RequestException as e:
+        # Catch all other exceptions.
+        return f"An unexpected error occurred: {e}"
 
 
 class Profile:
@@ -10,3 +34,17 @@ class Profile:
         self.name = "Emmanuel Adu Saah"
         self.stack = ["Python", "Django", "Flask", "Go/Gin"]
         self.timestamp = datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace('+00:00', 'Z')
+        self.facts = get_facts()
+
+    def profile_dict(self):
+        profile = {
+            "status": "success",
+            "user": {
+                "email": self.email,
+                "name": self.name,
+                "stack": self.stack
+            },
+            "timestamp": self.timestamp,
+            "fact": self.facts
+        }
+        return profile
