7650d81d6a18cc45d1ccf5ddea9f37d40aced6d0
kweku-annan <kwekuannan.dev@gmail.com>
2025-10-18 14:12:41 +0000

    Add Procfile for production. Add requirements.txt for requirements used

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from datetime import datetime, timezone
+import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
 
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    # Redirects to the '/me' route
+    return redirect(url_for('me'), code=301)
 
 @app.route('/me', methods=['GET'])
 def me():
     from models.profile import Profile
     profile = Profile()
     data = profile.profile_dict()
-
+    # print(data)
     return jsonify(data), 200
 
+# For Local testing
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     # Get port from environment variable.
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port, debug=True)
